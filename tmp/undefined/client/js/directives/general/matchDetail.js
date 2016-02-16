4011ed2bfccca906ff95a88235401eebb1d6626d
tqi <tom.qi@twc-Temp.com>
2016-02-16 11:07:14 -0800

    autocommit

--- tmp/undefined/client/js/directives/general/matchDetail.js
+++
@@ -1,1 +0,0 @@
-app.directive("lolChampionsMatchDetail",function(){return{restrict:"E",templateUrl:"templates/base/general/matchDetail.html",transclude:!0,replace:!0,scope:!1,link:function(a,b,c){},controller:function(a,b){b.getInfo("matchdetail").success(function(b){a.match=b}).error(function(a){console.log(a+"can't get summoner match details")})}}});
--- tmp/undefined/client/js/filter.js
+++
@@ -1,1 +0,0 @@
-app.filter("range",function(){return function(a,b){b=parseInt(b);for(var c=0;b>c;c++)a.push(c);return a}});
